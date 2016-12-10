fc04a9d78d3e13163a47db85fba5f5ed64ade7b1
Michał Chilczuk <michal.chilczuk@gmail.com>
2016-12-10 22:22:21 +0100

    Stub user agent.

--- PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/user/UserAgent.java
+++ PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/user/UserAgent.java
@@ -1,13 +1,20 @@
 package pl.edu.pw.eiti.wsd.printerweb.user;
 
-import jade.core.Agent;
+import java.util.Collection;
+
+import pl.edu.pw.eiti.wsd.printerweb.AgentRole;
+import pl.edu.pw.eiti.wsd.printerweb.RoleBasedAgent;
 
 /**
  * Agent which is responsible for interactions with {@link pl.edu.pw.eiti.wsd.printerweb.printer.PrinterAgent PrinterAgent}s on behalf of the user.
  */
-public class UserAgent extends Agent {
+public class UserAgent extends RoleBasedAgent {
 
     private static final long serialVersionUID = -3135506373065999424L;
 
-    
+    private Collection<? extends AgentRole> roles;
+
+    public UserAgent(Collection<? extends AgentRole> roles) {
+        super(roles);
+    }
 }
