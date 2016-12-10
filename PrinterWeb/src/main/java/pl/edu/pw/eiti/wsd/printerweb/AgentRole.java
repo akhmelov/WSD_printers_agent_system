796956fee6fde29a3b3d31e47589d03d7780127b
Michał Chilczuk <michal.chilczuk@gmail.com>
2016-12-10 22:33:57 +0100

    Clean up javadocs and names for agent role interface.

--- PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/AgentRole.java
+++ PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/AgentRole.java
@@ -5,7 +5,18 @@ import java.util.Collection;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 
+/**
+ * Role which may be applied to an Agent.
+ */
 public interface AgentRole {
-    
-    abstract Collection<Behaviour> getBehaviours(Agent userAgent);
+
+    /**
+     * Retrieves all behaviors of this role.
+     * 
+     * @param agent
+     *      Agent to which this role is applied. Not null.
+     * @return
+     *      All behaviors played by this role. Never null.
+     */
+    abstract Collection<Behaviour> getBehaviours(final Agent agent);
 }
