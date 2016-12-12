34beb5e69ba23b0ce3cdd0ac8ee64d12e1229569
Michał Chilczuk <michal.chilczuk@gmail.com>
2016-12-12 23:44:44 +0100

    remove old structure

--- common/src/main/java/wsd/printers/agent/common/enums/PaperFormatEnum.java
+++
@@ -1,8 +0,0 @@
-package wsd.printers.agent.common.enums;
-
-/**
- * Created by akhmelov on 11/1/16.
- */
-public enum PaperFormatEnum {
-    A1, A2, A3, A4, A5, A6, A7, A8, A9
-}
--- seller-agent-app/README.md
+++
@@ -1,4 +0,0 @@
-# WSD_printers_agent_system
-Printers' multiagent system (Wieloagentowy system drukarek)
-
-Project base on: https://bitbucket.org/stancalau/springfx
