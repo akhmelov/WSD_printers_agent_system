e6d797d348af48a03bda4b7abb558d7214652b3f
Michał Chilczuk <michal.chilczuk@gmail.com>
2016-12-15 22:44:31 +0100

    Stub printers map

---
+++ PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/printer/Location.java
@@ -0,0 +1,34 @@
+package pl.edu.pw.eiti.wsd.printerweb.printer;
+
+public class Location {
+
+    private int x;
+
+    private int y;
+
+    private int floor;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+}
---
+++ PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/printer/PrintersMap.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.eiti.wsd.printerweb.printer;
+
+import java.util.Set;
+
+import jade.core.AID;
+
+public class PrintersMap {
+
+    public Set<AID> getPrintersNearby(Location location) {
+        return null;
+    }
+}
