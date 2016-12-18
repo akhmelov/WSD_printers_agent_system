1116d93b796fb97295b1858e2004698fccd396ed
Michał Chilczuk <michal.chilczuk@gmail.com>
2016-12-18 21:03:44 +0100

    deleted unnecessary classes

--- PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/user/UserRole.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pw.eiti.wsd.printerweb.user;
-
-import pl.edu.pw.eiti.wsd.printerweb.AgentRole;
-
-
-public interface UserRole extends AgentRole {
-
-}
--- PrinterWeb/src/main/java/pl/edu/pw/eiti/wsd/printerweb/user/UserRoleImpl.java
+++
@@ -1,5 +0,0 @@
-package pl.edu.pw.eiti.wsd.printerweb.user;
-
-public class UserRoleImpl implements UserRole {
-
-}
